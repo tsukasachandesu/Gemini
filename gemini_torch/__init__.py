606f34a6325032c556992497f849be271af30bdf
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2024-08-26 00:42:29 +0900

    Update __init__.py

--- gemini_torch/__init__.py
+++ gemini_torch/__init__.py
@@ -1,10 +1,8 @@
 from gemini_torch.long_gemini import LongGeminiTransformerBlock, LongGemini
-from gemini_torch.model import Gemini
 from gemini_torch.tokenizer import MultimodalSentencePieceTokenizer
 
 __all__ = [
     "LongGeminiTransformerBlock",
     "LongGemini",
-    "Gemini",
     "MultimodalSentencePieceTokenizer",
 ]
